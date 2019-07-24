0d1f836bb7de1cff1411d9fe9bf551709064c963
Sominemo <me@sominemo.com>
2019-07-25 01:23:39 +0300

    appNavigation highlighter fix

--- Loaders/UI/Listeners.js
+++ Loaders/UI/Listeners.js
@@ -3,7 +3,7 @@ import { Nav } from "@Environment/Library/DOM/buildBlock"
 import Navigation from "@Core/Services/navigation"
 import PointerInfo from "@Core/Services/PointerInfo"
 
-Listeners.add(window, "appNavigation", (e) => { Nav.highlight({ id: e.parsed.module }) }, true)
+Listeners.add(window, "appNavigation", (e) => { Nav.highlight({ id: e.detail.parsed.module }) }, true)
 Listeners.add(window, "resize", () => Nav.updateGesturePosition(), true)
 Listeners.add(window, "popstate", () => Navigation.listener(), true)
 Listeners.add(window, "mousemove", e => PointerInfo.moveEventListener(e), true)
