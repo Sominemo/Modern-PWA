522dea306e2de616142891e018e28133ca9078dc
Sominemo <me@sominemo.com>
2020-03-04 17:17:46 +0200

    Fix Fonts API detection

--- Loaders/UI/loadFonts.js
+++ Loaders/UI/loadFonts.js
@@ -1,9 +1,11 @@
-import { CoreLoader } from "@Core/Init/CoreLoader"
+import { CoreLoader, CoreLoaderSkip, CoreLoaderResult } from "@Core/Init/CoreLoader"
 
 CoreLoader.registerTask({
     id: "fonts",
     presence: "Fonts loading",
     async task() {
+        if (!document.fonts) return new CoreLoaderSkip("Fonts API is not supported")
         await Promise.all(Array.from(document.fonts).map((font) => font.load().catch((e) => e)))
+        return new CoreLoaderResult()
     },
 })
