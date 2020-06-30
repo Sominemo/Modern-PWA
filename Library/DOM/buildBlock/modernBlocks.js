697627d3c1c5433d8574971db19ebb847e5297ed
Sominemo <me@sominemo.com>
2020-06-30 11:55:32 +0300

    ModernBlocks now resizes adaptively up to w <= 5

--- Library/DOM/buildBlock/modernBlocks.js
+++ Library/DOM/buildBlock/modernBlocks.js
@@ -8,10 +8,13 @@ export default class ModernBlocks {
 
     index = []
 
-    widgets = []
+    widgetsRAW = []
 
-    constructor({ centering = true, widgets = [] } = {}) {
-        this.widgets = widgets.filter((e) => !(e.item instanceof Error)).sort((a, b) => {
+    constructor({
+        centering = true, widgets = [], headerStyle = {}, headerContent = [], fixedHeight = true,
+        append = [], useClass = false,
+    } = {}) {
+        this.widgetsRAW = widgets.filter((e) => !(e.item instanceof Error)).sort((a, b) => {
             if (a.y < b.y) return -1
             if (a.y > b.y) return 1
 
@@ -19,31 +22,37 @@ export default class ModernBlocks {
             if (a.x > b.x) return 1
             return 0
         })
+        this.fixedHeight = fixedHeight
+
+        this.widgetsRAW.forEach((e) => {
+            e.item.controller = this
+        })
 
         this.header = new DOM({
             new: "div",
             class: ["win-modern-blocks-header"],
+            style: headerStyle,
+            content: headerContent,
         })
 
         this.content = new DOM({
             new: "div",
             class: ["win-modern-blocks-content"],
-            content: this.widgets.map((e) => e.item),
+            content: this.widgetsRAW.map((e) => e.item),
             style: {
                 margin: (centering ? "auto" : ""),
             },
         })
 
-        this.position()
-        let listenerObj
         const listener = () => this.position()
 
-        return new DOM({
+        this.object = new DOM({
             new: "div",
-            class: ["win-modern-blocks-container"],
+            class: ["win-modern-blocks-container", ...(fixedHeight ? [] : ["not-fixed-height"])],
             content: [
                 this.header,
                 this.content,
+                ...append,
             ],
             objectProperty: [
                 {
@@ -52,22 +61,58 @@ export default class ModernBlocks {
                 },
             ],
             onRendered() {
-                listenerObj = Design.onScaffoldModeUpdate(listener)
+                window.addEventListener("resize", listener, { passive: true })
             },
             onClear() {
-                listenerObj.removeListener(listener)
+                window.removeEventListener("resize", listener, { passive: true })
             },
         })
+
+        this.position()
+
+        if (!useClass) { return this.object }
+    }
+
+    get widgets() {
+        const res = []
+        this.widgetsRAW.forEach(({
+            item, x, y, actualX = null,
+        }, i) => {
+            const s = item.size
+            if (actualX === "next") x = res[i - 1].x + res[i - 1].sizeX
+            res.push({
+                item, x, y, sizeX: s.x, sizeY: s.y,
+            })
+        })
+        return res
     }
 
     get limit() {
-        return (Design.scaffoldMode === "mobile" ? 3 : null)
+        const min = 3
+
+        if (Design.scaffoldMode === "mobile") return min
+
+        const target = 5
+        const m = Design.vmin
+        const r = parseInt(Design.getVar("size-card-side-default", true), 10)
+        const g = parseInt(Design.getVar("size-block-gap", true), 10)
+        let w = this.object.elementParse.native.clientWidth
+        if (!w) {
+            const s = parseInt(Design.getVar("size-nav-width", true), 10)
+            w = window.innerWidth - 10 * m - s
+        }
+
+        const a = Math.floor((w + g * m) / (r * m + g * m))
+        if (a <= target) return a
+        return null
     }
 
     takenMap(widgets = this.autoReposition) {
         const map = {}
-        widgets.forEach(({ item, x, y }, ind) => {
-            const { size } = item
+        widgets.forEach(({
+            item, x, y, sizeX, sizeY,
+        }, ind) => {
+            const size = { x: sizeX, y: sizeY }
             for (let i = y; i < y + size.y; i++) {
                 if (!(i in map)) map[i] = {}
                 for (let j = x; j < x + size.x; j++) {
@@ -82,12 +127,17 @@ export default class ModernBlocks {
 
     checkFit({
         x, y, sizeX, sizeY,
-    }, map = this.takenMap()) {
-        if (x + sizeX - 1 > this.limit) return false
+    }, map = this.takenMap(), limit = this.limit, ignore = null) {
+        if (limit && x + sizeX - 1 > limit) return false
         for (let i = y; i < y + sizeY; i++) {
             if (!(i in map)) break
             for (let j = x; j < x + sizeX; j++) {
-                if (j in map[i]) return false
+                if (j in map[i]) {
+                    if (ignore !== null) {
+                        if (!(j >= ignore.x && j <= ignore.x + sizeX - 1
+                            && i >= ignore.y && i <= ignore.y + sizeY - 1)) return false
+                    } else return false
+                }
             }
         }
         return true
@@ -95,27 +145,34 @@ export default class ModernBlocks {
 
     get autoReposition() {
         const { limit } = this
-        if (limit === null) return this.widgets
+        if (limit === null || limit === 5) return this.widgets
 
         const repositioned = []
-        this.widgets.forEach(({ x, y, item }) => {
-            if (item.size.x > limit) return
+        this.widgets.forEach(({
+            x, y, item, sizeX, sizeY,
+        }) => {
+            const size = { x: sizeX, y: sizeY }
+            if (size.x > limit) return
             if (this.checkFit({
-                x, y, sizeX: item.size.x, sizeY: item.size.y,
-            }, this.takenMap(repositioned))) {
-                repositioned.push({ x, y, item })
+                x, y, sizeX: size.x, sizeY: size.y,
+            }, this.takenMap(repositioned), limit)) {
+                repositioned.push({
+                    x, y, item, sizeX, sizeY,
+                })
             } else {
                 let newX = 1
                 let newY = y + 1
                 while (!this.checkFit({
-                    x: newX, y: newY, sizeX: item.size.x, sizeY: item.size.y,
-                }, this.takenMap(repositioned))) {
+                    x: newX, y: newY, sizeX: size.x, sizeY: size.y,
+                }, this.takenMap(repositioned), limit)) {
                     if (newX === limit) {
                         newY++
                         newX = 1
                     } else newX++
                 }
-                repositioned.push({ x: newX, y: newY, item })
+                repositioned.push({
+                    x: newX, y: newY, item, sizeX, sizeY,
+                })
             }
         })
 
@@ -125,23 +182,27 @@ export default class ModernBlocks {
     position() {
         this.x = 1
         this.y = 1
-        this.autoReposition.forEach(({ item, x, y }) => {
+        this.autoReposition.forEach(({
+            item, x, y, sizeX, sizeY,
+        }) => {
+            const size = { x: sizeX, y: sizeY }
             item.style({
                 gridColumnStart: x,
                 gridRowStart: y,
-                gridColumnEnd: `span ${item.size.x}`,
-                gridRowEnd: `span ${item.size.y}`,
+                gridColumnEnd: `span ${size.x}`,
+                gridRowEnd: `span ${size.y}`,
             })
-            item.controller = this
-            if (x + item.size.x - 1 > this.x) this.x = x + item.size.x - 1
-            if (y + item.size.y - 1 > this.y) this.y = y + item.size.y - 1
+            if (x + size.x - 1 > this.x) this.x = x + size.x - 1
+            if (y + size.y - 1 > this.y) this.y = y + size.y - 1
         })
 
         this.content.style({
             gridTemplateColumns: `repeat(${this.x}, var(--size-card-side))`,
-            gridTemplateRows: `repeat(${this.y}, var(--size-card-side))`,
+            ...(this.fixedHeight ? { gridTemplateRows: `repeat(${this.y}, var(--size-card-side))` } : {}),
         })
     }
+
+    requestCache = {}
 
     async dataRequest(scopes) {
         return scopes.map(
