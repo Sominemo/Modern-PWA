911155bc00462c509b781b2aa1e53eb1d5aba84a
Sominemo <me@sominemo.com>
2020-06-30 11:56:17 +0300

    Widget tweaks

--- Library/DOM/elements/block.js
+++ Library/DOM/elements/block.js
@@ -11,14 +11,24 @@ export default class Block {
         const icon = props.icon || "dashboard"
         const colors = props.colors || { main: "#727272", light: "#bcbfc1" }
         const data = props.data || {}
+        const request = props.request || []
         const size = props.size || { x: 1, y: 1 }
+        const setup = props.setup || (() => {})
+        const name = props.name || "Widget"
         return new DOM({
             new: "modern-block",
             objectProperty: [
                 {
                     name: "props",
-                    // eslint-disable-next-line prefer-rest-params
                     handler: props,
+                },
+                {
+                    name: "name",
+                    handler: name,
+                },
+                {
+                    name: "setup",
+                    handler: setup,
                 },
                 {
                     name: "controller",
@@ -34,11 +44,15 @@ export default class Block {
                     name: "size",
                     handler: size,
                 },
+                {
+                    name: "colors",
+                    handler: colors,
+                },
             ],
             async onRendered(ev, el) {
                 const d = (el.controller
-                    ? await el.controller.dataRequest(el.props.data)
-                    : {})
+                    ? await el.controller.dataRequest(request)
+                    : [])
                 const result = await render(el, d)
                 const body = new DOM({
                     new: "modern-block-body",
@@ -62,7 +76,7 @@ export default class Block {
                                     )
                                     || [142, 142, 142]).join(",")
                                 el.style({
-                                    boxShadow: `0px 0.5vmin 0.7vmin rgba(${color},0.05), 0px 1vmin 2vmin rgba(${color},0.1)`,
+                                    "--shadow-block-color": color,
                                 })
                             }, 200)
                         })
@@ -80,7 +94,7 @@ export default class Block {
                             left: "50%",
                             top: "50%",
                             transform: "translate(-50%, -50%)",
-                            fontSize: "6vmin",
+                            fontSize: "2em",
                             color: colors.main,
                         }),
                         new DOM({
